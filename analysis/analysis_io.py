a7a27d6bf850e0b7a505f53ffb2b84a75f32f2ba
Tim @ LEAP <a@b>
2025-09-29 07:45:30 +0000

    created function for opening ar6 projections

--- analysis/analysis_io.py
+++ analysis/analysis_io.py
@@ -1,5 +1,6 @@
 import xarray as xr
 import gcsfs
+import os
 fs = gcsfs.GCSFileSystem()
 
 def open_zos_ibe_monmeans(zos_path,ibe_path,chunks):
@@ -28,3 +29,16 @@ def open_aslc_amax(input_path,chunks):
     fns = ['gs://'+k for k in fs.ls(input_path) if 'amax' in k]
     ds = xr.open_mfdataset(fns,combine='nested',coords='minimal',compat='override',concat_dim='experiment_id',chunks=chunks,engine='zarr')
     return ds
+
+def open_anmax(input_path,chunks):
+    #open annual maxima stored per source_id
+    fns = ['gs://'+k for k in fs.ls(input_path) if 'DS_Store' not in k]
+    return xr.open_mfdataset(fns,combine='nested',coords='minimal',compat='override',concat_dim = 'source_id',chunks=chunks,engine='zarr')
+
+def open_ar6_projections(input_path,grid_type,component):
+    
+    fns = [os.path.join(input_path,k) for k in os.listdir(input_path) if grid_type in k and component in k]
+    ar6_projections = xr.open_mfdataset(fns,combine='nested',concat_dim='experiment_id').sea_level_change/10 #convert from mm to cm
+    ar6_projections['experiment_id'] = [k.split('/')[-1].split('_')[1] for k in fns]
+    
+    return ar6_projections #in cm
